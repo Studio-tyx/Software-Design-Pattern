5e5ae3b01869f3b13fd743dd1ab353527120f795
studio-tyx <1341913265@qq.com>
2021-04-21 15:10:35 +0800

    20210421
    把命令模式最后的计算器题码了（但是还是有点不明觉厉<-这种东西还要命令模式单独写一个Command吗）

---
+++ DesignPattern/src/lesson_20210419/Calculator/User.java
@@ -0,0 +1,45 @@
+package lesson_20210419.Calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TYX
+ * @name User
+ * @description 用户界面（封装了多层回退+重做）
+ * @createTime 2021/4/21 14:51
+ **/
+public class User {
+    private Calculator _calculator=new Calculator();
+    private List<Command> _commands=new ArrayList<>();
+    private int _current=0;
+
+    public void redo(int levels){
+        System.out.println("\n-----redo "+levels+" levels");
+        for(int i=0;i<levels;i++){
+            if(_current<_commands.size()-1){
+                Command command=_commands.get(_current-1);
+                _current++;
+                command.execute();
+            }
+        }
+    }
+
+    public void undo(int levels){
+        System.out.println("\n-----undo "+levels+" levels");
+        for(int i=0;i<levels;i++){
+            if(_current>0){
+                Command command=_commands.get(_current-1);
+                _current--;
+                command.unExecute();
+            }
+        }
+    }
+
+    public void compute(char operator,int operand){
+        Command command=new CalculatorCommand(operator,operand,_calculator);
+        command.execute();
+        _commands.add(command);
+        _current++;
+    }
+}
